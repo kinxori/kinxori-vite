626f985c8b7016a359d167012c4879a0219f3cca
kinxori <116584982+kinxori@users.noreply.github.com>
2023-03-16 17:10:55 -0600

    base added

--- my_web_portfolio/vite.config.js
+++ my_web_portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myportfolio"
+  base: "https://kinxori.github.io/myportfolio"
 })
