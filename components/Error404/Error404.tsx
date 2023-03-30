ae4cc60212ba4a270baee9dfbacc94dad9adc78f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-03-30 02:32:15 -0600

    404 image path fixed to public

--- components/Error404/Error404.tsx
+++ components/Error404/Error404.tsx
@@ -2,13 +2,15 @@ import { Link, useRouteError } from "react-router-dom"
 import "./_Error404.css"
 import Button from "../Buttons/Button"
 
+const img404 = "/404Assets.png"
+
 
 export default function Error404(){
     const error:any = useRouteError()
 
 return(
     <article id="error-404">
-        <img src="./components/Error404/404Assets.png" alt="404error" />
+        <img src={img404} alt="404error" />
         <span>
             <i> {error.message || error.statusText}</i> 🥸
         </span>     
