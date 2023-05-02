c6f89c4b47f494e1cea2f6e532ef2f0d9770179c
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-02 16:06:27 -0600

    animations created, landing page done

--- components/Error404/Error404.tsx
+++ components/Error404/Error404.tsx
@@ -1,22 +1,21 @@
-import { Link, useRouteError } from "react-router-dom"
-import "./_Error404.css"
-import Button from "../Buttons/Button"
+import { Link, useRouteError } from "react-router-dom";
+import "./_Error404.css";
+import Button from "../Buttons/Button";
 
-const img404 = "/404Assets.png"
+const img404 = "/404Assets.png";
 
+export default function Error404() {
+  const error: any = useRouteError();
 
-export default function Error404(){
-    const error:any = useRouteError()
-
-return(
+  return (
     <article id="error-404">
-        <img src={img404} alt="404error" />
-        <span>
-            <i> {error.message || error.statusText}</i> 🥸
-        </span>     
-        <Button variant="mainButton" to="/" className="error404-btn">
-            Back home 
-        </Button>
+      <img src={img404} alt="404error" />
+      <span>
+        <i> {error.message || error.statusText}</i> 🥸
+      </span>
+      <Button variant="mainButton" to="/" className="error404-btn">
+        Back home
+      </Button>
     </article>
-)
+  );
 }
--- routes/LandingPage/LandingPage.tsx
+++ routes/LandingPage/LandingPage.tsx
@@ -1,24 +1,31 @@
-import Button from '../../components/Buttons/Button'
-import './_landingPage.css'
+import Button from "../../components/Buttons/Button";
+import "./_landingPage.css";
 
-export default function LandingPage(){
-    return(
-        <article className='landingPage-root'>
-            <div className='landingPage-content'>
-                <img src="/AstroAssets.png" alt="astro-riding-on-bike"/>
-                <div className='landingPage-copy'>
-                    <h2>
-                        Combining <b>creativity, numbers,</b> and <b>code</b> to make things happen 🚀
-                    </h2>
-                    <br/>
-                    <p>
-                        Hi! I am Kinxori and <b>welcome</b> to my website! Developing myself as a <b>web developer</b> and <b>motion graphic artist.</b> Take a deep dive into my work!
-                    </p>
-                    <Button variant="secButton" to="/projects" className='landingPage-CTA'>
-                         Go projects
-                    </Button>
-                </div>
-            </div>
-        </article>
-    )
+export default function LandingPage() {
+  return (
+    <article className="landingPage-root">
+      <div className="landingPage-content">
+        <img src="/AstroAssets.png" alt="astro-riding-on-bike" />
+        <div className="landingPage-copy">
+          <h2>
+            Combining <b>creativity, numbers,</b> and <b>code</b> to make things
+            happen 🚀
+          </h2>
+          <br />
+          <p>
+            Hi! I am Kinxori and <b>welcome</b> to my website! Developing myself
+            as a <b>web developer</b> and <b>motion graphic artist.</b> Take a
+            deep dive into my work!
+          </p>
+          <Button
+            variant="secButton"
+            to="/projects"
+            className="landingPage-CTA"
+          >
+            Go projects
+          </Button>
+        </div>
+      </div>
+    </article>
+  );
 }
