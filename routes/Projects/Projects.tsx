89448a5d8e4296ebbf0c79a4b0428c00d4cf2cba
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-12 23:35:10 -0600

    projects css updated - added h2

--- routes/Projects/Projects.tsx
+++ routes/Projects/Projects.tsx
@@ -85,7 +85,7 @@ export default function Projects() {
                 )}
                 <video
                   src="https://firebasestorage.googleapis.com/v0/b/myportfolio-70cb1.appspot.com/o/shopping-cart-functionality-video.mov?alt=media&token=324db9e4-dc38-4227-a574-a3a9080f2797"
-                  onLoadedData={() => setIsLoadingVideo(true)}
+                  onLoadedData={() => setIsLoadingVideo(false)}
                   loop
                   autoPlay
                   controls={false}
@@ -93,11 +93,7 @@ export default function Projects() {
                 ></video>
                 <div></div>
               </div>
-
-              <h3>
-                <i className="fa-solid fa-cart-shopping"></i> Shopping Cart
-              </h3>
-
+              <h2>Shopping Cart</h2>
               <p>
                 This feature updates the shopping cart's total price based on user input quantity
                 and removes items from the cart.
@@ -153,178 +149,91 @@ export default function Projects() {
                 </Button>
               </div>
             </div>
-          </DemoCard>
-          <DemoCard
-            header={
-              <>
-                <i className="fa-solid fa-cart-shopping"></i> Shopping Cart
-              </>
-            }
-            isActive={isClicked === "card1"}
-            onClick={() => handleCardClick("card1")}
-          >
-            <div
-              className={
-                isFullScreen === "1"
-                  ? "projects-full-screen-asset-open"
-                  : "projects-full-screen-asset-close"
-              }
-              onClick={() => handleFullScreen("1")}
-            >
-              <Button variant="linkButton">Close</Button>
-              {isLoadingVideo && <h3 className="projects-full-screen-loader">Loading video...𛱘</h3>}
-              <video
-                src="https://firebasestorage.googleapis.com/v0/b/myportfolio-70cb1.appspot.com/o/shopping-cart-functionality-video.mov?alt=media&token=324db9e4-dc38-4227-a574-a3a9080f2797"
-                onLoadedData={() => setIsLoadingVideo(false)}
-                loop
-                autoPlay
-                controls={false}
-                muted
-              ></video>
-              <div></div>
-            </div>
-            <h3>How does it work? 🤔</h3>
-            <p>
-              This feature updates the shopping cart's total price based on user input quantity and
-              removes items from the cart.
-            </p>
-            <h3>Technologies used 💡</h3>
-            <ul>
-              <li>
-                <i className="fa-brands fa-react"></i> React
-              </li>
-              <li>
-                <i className="fa-brands fa-square-js"></i> Javascript
-              </li>
-              <li>
-                <i className="fa-brands fa-html5"></i> HTML
-              </li>
-              <li>
-                <i className="fa-brands fa-css3"></i> CSS
-              </li>
-              <li>
-                <i className="fa-brands fa-node"></i> NodeJS
-              </li>
-            </ul>
-            <h3>Skills 🧠</h3>
-            <ul>
-              <li>State management</li>
-              <li>React component architecture</li>
-              <li>User experience and interactivity</li>
-              <li>Testing and debugging</li>
-            </ul>
-            <h3>Advantages 🦾</h3>
-            <ul>
-              <li>Improved user experience</li>
-              <li>Increased sales</li>
-              <li>Customizable</li>
-              <li>Easy integration</li>
-              <li>Reusability</li>
-              <li>Analytics</li>
-            </ul>
-            <h3>Difficulties ⚙️</h3>
-            <ul>
-              <li>State management and data flow</li>
-              <li>Handle user input and form data </li>
-              <li>Improve user experience with better organization of information</li>
-              <li>Integratation with external APIs to retrieve and update product information</li>
-            </ul>
-            <div className="projects-display-features-buttons-CTA">
-              <Button to="/contact">Contact me!</Button>
-              <ButtonAnchor
-                variant="secButton"
-                href="/FrontEnd - Gustavo Quiroz - Resume.pdf"
-                download
+            <div className="projects-display-card">
+              <div
+                className={
+                  isFullScreen === "search-bar-asset"
+                    ? "projects-full-screen-asset-open"
+                    : "projects-full-screen-asset-close"
+                }
+                onClick={() => handleFullScreen("search-bar-asset")}
               >
-                Download Resume
-              </ButtonAnchor>
-            </div>
-          </DemoCard>
-          <DemoCard
-            header={
-              <>
+                <Button variant="linkButton">Close</Button>
+                {isLoadingVideo && (
+                  <h3 className="projects-full-screen-loader">Loading video...𛱘</h3>
+                )}
+                <video
+                  src="https://firebasestorage.googleapis.com/v0/b/myportfolio-70cb1.appspot.com/o/search%20bar%20edited.mov?alt=media&token=4cf3a15b-8a9d-4548-8590-9602af1d62d4"
+                  onLoadedData={() => setIsLoadingVideo(false)}
+                  loop
+                  autoPlay
+                  controls={false}
+                  muted
+                ></video>
+                <div></div>
+              </div>
+              <h3>
                 <i className="fa-solid fa-magnifying-glass"></i> Search Bar
-              </>
-            }
-            isActive={isClicked === "card2"}
-            onClick={() => handleCardClick("card2")}
-          >
-            <div
-              className={
-                isFullScreen === "2"
-                  ? "projects-full-screen-asset-open"
-                  : "projects-full-screen-asset-close"
-              }
-              onClick={() => handleFullScreen("2")}
-            >
-              <Button variant="linkButton">Close</Button>
-              {isLoadingVideo && <h3 className="projects-full-screen-loader">Loading video...𛱘</h3>}
-              <video
-                src="https://firebasestorage.googleapis.com/v0/b/myportfolio-70cb1.appspot.com/o/search%20bar%20edited.mov?alt=media&token=4cf3a15b-8a9d-4548-8590-9602af1d62d4"
-                onLoadedData={() => setIsLoadingVideo(false)}
-                loop
-                autoPlay
-                controls={false}
-                muted
-              ></video>
-              <div></div>
-            </div>
-            <h3>How does it work? 🤔</h3>
-            <p>
-              This is a search bar component that filters and displays data in a table based on user
-              input.
-            </p>
-            <h3>Technologies used 💡</h3>
-            <ul>
-              <li>
-                <i className="fa-brands fa-react"></i> React
-              </li>
-              <li>
-                <i className="fa-brands fa-square-js"></i> Javascript
-              </li>
-              <li>
-                <i className="fa-brands fa-html5"></i> HTML
-              </li>
-              <li>
-                <i className="fa-brands fa-css3"></i> CSS
-              </li>
-              <li>
-                <i className="fa-brands fa-node"></i> NodeJS
-              </li>
-            </ul>
-            <h3>Skills 🧠</h3>
-            <ul>
-              <li>Filtering and searching data</li>
-              <li>Componentization and modularity</li>
-              <li>Handling events and state changes</li>
-            </ul>
-            <h3>Advantages 🦾</h3>
-            <ul>
-              <li>Allows users to easily search and filter large datasets</li>
-              <li>Provides real-time search results as users type</li>
-              <li>
-                Improves user experience by reducing the need to manually search and sort through
-                data
-              </li>
-            </ul>
-            <h3>Difficulties ⚙️</h3>
-            <ul>
-              <li>Managing state</li>
-              <li>Filtering logic</li>
-              <li>Handling large datasets</li>
-              <li>Accessibility</li>
-            </ul>
-            <div className="projects-display-features-buttons-CTA">
-              <Button to="/contact">Contact me!</Button>
-              <ButtonAnchor
-                variant="secButton"
-                href="/FrontEnd - Gustavo Quiroz - Resume.pdf"
-                download
-              >
-                Download Resume
-              </ButtonAnchor>
+              </h3>
+              <p>
+                This is a search bar component that filters and displays data in a table based on
+                user input.
+              </p>
+              <h3>Technologies used 💡</h3>
+              <ul>
+                <li>
+                  <i className="fa-brands fa-react"></i> React
+                </li>
+                <li>
+                  <i className="fa-brands fa-square-js"></i> Javascript
+                </li>
+                <li>
+                  <i className="fa-brands fa-html5"></i> HTML
+                </li>
+                <li>
+                  <i className="fa-brands fa-css3"></i> CSS
+                </li>
+                <li>
+                  <i className="fa-brands fa-node"></i> NodeJS
+                </li>
+              </ul>
+              <h3>Skills 🧠</h3>
+              <ul>
+                <li>Filtering and searching data</li>
+                <li>Componentization and modularity</li>
+                <li>Handling events and state changes</li>
+              </ul>
+              <h3>Advantages 🦾</h3>
+              <ul>
+                <li>Allows users to easily search and filter large datasets</li>
+                <li>Provides real-time search results as users type</li>
+                <li>
+                  Improves user experience by reducing the need to manually search and sort through
+                  data
+                </li>
+              </ul>
+              <h3>Difficulties ⚙️</h3>
+              <ul>
+                <li>Managing state</li>
+                <li>Filtering logic</li>
+                <li>Handling large datasets</li>
+                <li>Accessibility</li>
+              </ul>
+              <div className="projects-display-cards-CTA">
+                <Button to="https://kinxori.github.io/react-practice-5/" target="_blank">
+                  Live Project
+                </Button>
+                <Button
+                  to="https://github.com/kinxori/react-practice-5"
+                  variant="secButton"
+                  target="_blank"
+                >
+                  Github Repo
+                </Button>
+              </div>
             </div>
           </DemoCard>
+
           <DemoCard
             header={
               <>
