0b988970459e974632ce71824389bb47ee2b62c0
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-15 00:02:59 -0600

    adding normalization for <b> font weigth 700

--- routes/AboutMe/AboutMe.tsx
+++ routes/AboutMe/AboutMe.tsx
@@ -32,7 +32,7 @@ export default function AboutMe() {
 
           <p>
             My name is Gustavo Quiroz, <b>front-end web developer</b> with a passion for creating
-            things. Formerly in <b>motion graphics,</b> now using my love for creating to{" "}
+            things. Formerly in <b>motion design,</b> now using my love for creating to{" "}
             <b>solve problems.</b>
           </p>
           <Button className="aboutMe-CTA" variant="secButton" onClick={handleClick}>
