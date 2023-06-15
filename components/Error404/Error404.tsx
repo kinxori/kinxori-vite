57d2cb92629146c220c1f19709ab4c7737ed57c6
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-14 23:10:44 -0600

    img asset for 404 error page updated with an importation

--- components/Error404/Error404.tsx
+++ components/Error404/Error404.tsx
@@ -1,15 +1,14 @@
-import { Link, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 import "./_Error404.css";
 import Button from "../Buttons/Button";
-
-const img404 = "/404Assets.png";
+import img404 from "./../../assets/404Assets.png";
 
 export default function Error404() {
   const error: any = useRouteError();
 
   return (
     <article className="error-404-root">
-      <img src={img404} alt="404error-asset" />
+      <img src={img404} alt="" />
       <h2>
         <i> {error.message || error.statusText}</i> 🥸
       </h2>
