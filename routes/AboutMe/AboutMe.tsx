d4899278e09aa51ce086b7bab9d88ec5447966f1
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-17 01:09:01 -0600

    updated about page content and tablet media query css

--- routes/AboutMe/AboutMe.tsx
+++ routes/AboutMe/AboutMe.tsx
@@ -32,9 +32,9 @@ export default function AboutMe() {
           </h2>
 
           <p>
-            My name is Gustavo Quiroz, <b>front-end web developer</b> with a passion for creating
-            things. Formerly a <b>motion design,</b> now using my love for creating to
-            <b> solve problems.</b>
+            My name is Gustavo Quiroz, <b> front-end web developer </b> with a passion for creating
+            things. Formerly a <b> motion design, </b> now using my love for creating to
+            <b> solve problems. </b>
           </p>
           <Button className="aboutMe-CTA" variant="secButton" onClick={handleClick}>
             {isOpen ? "Read less" : "Read more"}
@@ -46,63 +46,79 @@ export default function AboutMe() {
           <div className="aboutme-copy-PFP">
             <img src={imgAssetPFP} alt="Gustavo Quiroz - Kinxori"></img>
           </div>
-          <h3>🌎 Where do I come from?</h3>
+          <h2>🌎 Where do I come from?</h2>
           <p>
-            Born in 1998, I come from a small town near the central region of Mexico. Despite its
-            size, the town has a rich cultural heritage, with vibrant music and dance traditions,
-            colorful festivals, and delicious local cuisine. Growing up there has taught me the
-            value of community, hard work, and creativity, which I carry with me wherever I go.
+            <b> Born in 1998, </b> I come from a small town near the central region of
+            <b> Mexico. </b>
+            Despite its size,the town has a <b>rich cultural heritage,</b>
+            with vibrant music and dance traditions, colorful festivals, and delicious local
+            cuisine. Growing up there
+            <b> has taught me </b> the value of
+            <b> community, hard work, and creativity, </b>
+            <b> which I carry with me </b> wherever I go.
           </p>
-          <h3>💆 Personality?</h3>
+          <h2>💆 Personality?</h2>
           <p>
-            I'm a person who enjoys a balanced lifestyle. While I'm not necessarily extroverted, I
-            enjoy my time at home. I find peace and serenity in my surroundings and like to make the
-            most of it. However, that doesn't mean I don't like going out. I enjoy having dinner
-            with friends and engaging in deep conversations. When I'm not socializing, I like to
-            study and listen to music. I strive to live a meaningful life, and I'm always looking
-            for ways to improve myself and my surroundings.
+            I'm a person who enjoys a <b> balanced lifestyle. </b> While I'm not necessarily
+            extroverted, I enjoy my time at home. I find peace and serenity in my surroundings and
+            like to
+            <b> make the most of it. </b> However, that doesn't mean I don't like going out.
+            <b> I enjoy having dinner with friends </b> and engaging in deep conversations. When I'm
+            not socializing, I like to
+            <b> study and listen to music. </b> I strive to live a meaningful life, and I'm
+            <b> always looking for ways to improve myself </b> and my surroundings.
           </p>
-          <h3>👨‍🎓 Career?</h3>
+          <h2>👨‍🎓 Career?</h2>
           <p>
-            Throughout my career path, I have pursued my passion for creativity and visual arts.
-            After graduating with a degree in Animation and Visual Effects in 2020, I landed a job
-            as a 2D Cut-Out Animator and Background Illustrator from 2020-2021. This was just the
-            start of my journey, as I eventually progressed to become a Lead Motion Graphic Artist
-            in a Canadian marketing agency in mid-2021.
-          </p>
-          <p>
-            Although I had a successful career in the visual arts industry, I felt the need to learn
-            new skills and broaden my horizons. This is why in October 2022, I decided to start
-            learning web development. My passion for problem-solving and creating meaningful
-            experiences through web design grew, and in January 2023, I made the decision to pursue
-            web development as a full-time job.
+            Throughout my career path, I have pursued my
+            <b> passion for creativity and visual arts. </b>
+            After graduating with a degree in
+            <b> Animation and Visual Effects in 2020, </b> I landed a job as a 2D Cut-Out Animator
+            and Background Illustrator from 2020-2021. This was just the start of my journey, as I
+            eventually progressed to become a<b> Lead Motion Designer </b> in a Canadian marketing
+            agency in <b> August, 2021. </b>
           </p>
           <p>
-            Currently, I am focusing on developing myself as a Front-End Developer, and I am
-            determined to become a Full-Stack Developer in the near future. My career journey has
-            been an exciting and fulfilling experience, and I look forward to what the future holds
-            as I continue to grow and evolve as a developer.
+            Although I had a successful career in the visual arts industry,
+            <b>I felt the need to learn new skills and broaden my horizons.</b>
+            This is why in
+            <b> October 2022, </b> I decided to
+            <b> start learning web development. </b> My passion for
+            <b> problem-solving </b> and creating meaningful experiences through
+            <b> web design </b>
+            grew, and in <b> January 2023, </b> I made the decision to
+            <b> pursue web development as a full-time job. </b>
           </p>
-          <h3>🍿 Hobbies?</h3>
           <p>
-            Talking about hobbies, biking and tennis are my favorite sport activities, I also enjoy
-            going to the beach and spending time in nature. When I'm not outside, I enjoy exploring
-            my creative side through music and craftsmanship. I play guitar and am interested in
-            learning the violin and saxophone. I'm also interested in investing and learning about
-            global economy and tendencies in the market. Overall, I'm someone who likes to keep
-            learning and enjoy life's simple pleasures.
+            Currently, I am focusing on developing myself as a<b> Front-End Developer, </b> and I am
+            determined to become a<b> Full-Stack Developer </b> in the near future. My career
+            journey has been an exciting and fulfilling experience, and I look forward to what the
+            future holds as I continue to
+            <b> grow and evolve as a developer. </b>
           </p>
-          <h3>⏳ Aspirations?</h3>
+          <h2>🍿 Hobbies?</h2>
           <p>
-            My main aspiration in life is to achieve financial freedom, not just for myself, but
-            also to share a comfortable life with my loved ones. I believe that personal growth is
-            important, and I am always eager to learn new things and gain knowledge to help me reach
-            my goals. One of my passions is exploring new places and experiencing different
-            cultures, which is why I aspire to live a fulfilling life where I can travel and create
-            unforgettable memories with those closest to me. Ultimately, what I strive for is a
-            peaceful and fulfilling life, where I can enjoy the simple things in life and cherish
-            the moments spent with the people who matter most to me.
+            Talking about hobbies, <b> biking and tennis are my favorite sport activities, </b> I
+            also enjoy <b> going to the beach </b> and spending time in nature. When I'm not
+            outside, I enjoy exploring my creative side through <b> music and craftsmanship. </b> I
+            play guitar and am interested in <b> learning the violin and saxophone. </b> I'm also
+            <b> interested in investing </b> and learning about global economy and tendencies in the
+            market. Overall, I'm someone who likes to
+            <b> keep learning and enjoy life's simple pleasures. </b>
           </p>
+          <h2>⏳ Aspirations?</h2>
+          <p>
+            My main aspiration in life is to <b> achieve financial freedom, </b> not just for
+            myself, but also to <b> share a comfortable life with my loved ones. </b> I believe that
+            personal growth is important, and I am <b> always eager to learn new things </b> and
+            gain knowledge to help me <b> reach my goals. </b> One of my passions is exploring new
+            places and <b> experiencing different cultures, </b> which is why I aspire to live a
+            fulfilling life where I can travel and <b> create unforgettable memories </b> with those
+            closest to me. Ultimately, what <b> I strive for is a peaceful and fulfilling life, </b>
+            where I can enjoy the simple things in life and cherish the moments
+            <b> spent with the people who matter most to me. </b>
+          </p>
+          <i>17th of June, 2023 / Mexico</i>
         </div>
       )}
     </article>
