e3357be13c9a5cced4202cb23dbaf3f65235e918
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-27 20:21:25 -0600

    updated Home file - removed Outlet from code

--- routes/Home/Home.tsx
+++ routes/Home/Home.tsx
@@ -1,16 +1,31 @@
-import { Outlet, useLocation } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar"
-import LandingPage from '../LandingPage/LandingPage'
-import './_home.css'
+import { useLocation } from "react-router-dom";
+import NavBar from "./../../components/NavBar/NavBar";
+import LandingPage from "./../LandingPage/LandingPage";
+import "./_home.css";
+import Projects from "../Projects/Projects";
+import MotionProjects from "../motion projects/MotionProjects";
+import Contact from "../Contact/Contact";
+import AboutMe from "../AboutMe/AboutMe";
 
-export default function Home(){
+export default function Home() {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    return (
-        <section className="rootHome">
-            <NavBar/>
-            {location.pathname !== "/" ? <Outlet/> : <LandingPage/>}
-        </section>
-    )
+  return (
+    <section className="rootHome">
+      <NavBar />
+      {location.pathname === "/" ? (
+        <LandingPage />
+      ) : location.pathname === "/webprojects" ? (
+        <Projects />
+      ) : location.pathname === "/motionprojects" ? (
+        <MotionProjects />
+      ) : location.pathname === "/contact" ? (
+        <Contact />
+      ) : location.pathname === "/about" ? (
+        <AboutMe />
+      ) : (
+        <LandingPage />
+      )}
+    </section>
+  );
 }
