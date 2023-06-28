f8ab50ade9ac05c55f5075d91eb38c7b6e3a123e
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-27 20:26:47 -0600

    updated importation in Home page

--- routes/Home/Home.tsx
+++ routes/Home/Home.tsx
@@ -1,11 +1,11 @@
 import { useLocation } from "react-router-dom";
+import "./_home.css";
 import NavBar from "./../../components/NavBar/NavBar";
 import LandingPage from "./../LandingPage/LandingPage";
-import "./_home.css";
-import Projects from "../Projects/Projects";
-import MotionProjects from "../motion projects/MotionProjects";
-import Contact from "../Contact/Contact";
-import AboutMe from "../AboutMe/AboutMe";
+import Projects from "./../Projects/Projects";
+import MotionProjects from "./../motion projects/MotionProjects";
+import Contact from "./../Contact/Contact";
+import AboutMe from "./../AboutMe/AboutMe";
 
 export default function Home() {
   const location = useLocation();
