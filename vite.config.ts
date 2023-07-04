0df2cfc47f83476ee9d8699de20e18f4d432b50f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-03 22:07:12 -0600

    fixed bug of importations - modified vite.config and ts.config to handle custom paths importations

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    alias: {
+      "@src": "./src",
+      "@components": "./components",
+      "@routes": "routes",
+    },
+  },
 });
