336cc11b33245cf68ea5b60833f41d08bfbc9622
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-03 22:31:15 -0600

    updated erro404 content - replaced relative paths for custom paths importations

--- components/Error404/Error404.tsx
+++ components/Error404/Error404.tsx
@@ -1,7 +1,7 @@
 import { useRouteError } from "react-router-dom";
 import "./_Error404.css";
-import Button from "../Buttons/Button";
-import img404 from "/src/assets/404Assets.png";
+import Button from "@components/Buttons/Button";
+import img404 from "@src/assets/404Assets.png";
 
 export default function Error404() {
   const error: any = useRouteError();
