628e566d060e80ede80cb984aee3c47fc8401918
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-04 20:48:43 -0600

    fixed huitzi asset fullsreen behavior in motion projects page

--- routes/motionProjects/MotionProjects.tsx
+++ routes/motionProjects/MotionProjects.tsx
@@ -197,12 +197,12 @@ export default function MotionProjects() {
               <img
                 src={huitziTemple}
                 className={
-                  fullScreenAsset === "golden-kemonito-asset"
+                  fullScreenAsset === "huitzi-asset"
                     ? "motion-projects-full-screen-img-open"
                     : "motion-projects-full-screen-img-close"
                 }
                 alt="Golden Kemonito. 3D model created by Kinxori. A tribute to world famous Mexican wrestler, now converted into a golden statue. This character is wearing a blur fur suit, posing as if it was surfing, dark background with the iconic CMLL letters from the 'Comisión Mexicana de Lucha Libre' - kinxori.com "
-                onClick={() => handleFullScreen("golden-kemonito-asset")}
+                onClick={() => handleFullScreen("huitzi-asset")}
               ></img>
               <h2>Templo de Huitzilopochtli</h2>
               <p>
