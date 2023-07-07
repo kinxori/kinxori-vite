137f356609b65321f3852447e3b06d3f5a42fc12
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-06 18:07:32 -0600

    updating demo card content - fixed onClick to work in all the card

--- components/DemoCard/DemoCard.tsx
+++ components/DemoCard/DemoCard.tsx
@@ -2,12 +2,11 @@ import "./_demoCard.css";
 
 export default function DemoCard({ header, children, isActive, onClick }: any) {
   return (
-    <div className={isActive === true ? "card-root-open" : "card-root-close"} onClick={onClick}>
-      <div className="card-headline">
+    <div className={isActive === true ? "card-root-open" : "card-root-close"}>
+      <div className="card-headline" onClick={onClick}>
         <h3 className="card-headline-title">{header}</h3>
         <div>
           <span
-            onClick={onClick}
             className={isActive === true ? "card-root-chevron-open" : "card-root-chevron-close"}
           >
             <i className="fa-solid fa-chevron-right"></i>
