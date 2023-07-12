faf2de13f3bab10cc1090f8832b5cc4ff455178b
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-11 19:17:39 -0600

    updating mobileNav component - changing routing button strcuture with less code lines

--- src/components/NavBar/MobileNav.tsx
+++ src/components/NavBar/MobileNav.tsx
@@ -1,48 +1,55 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import "./_NavBar.css";
-
-function PathIcon(props: any) {
-  const { to, children, handleClick, target } = props;
-  const location = useLocation();
-
-  const handleClickWrapper = () => {
-    handleClick(location);
-  };
-
-  return (
-    <Link to={to} onClick={handleClickWrapper} target={target}>
-      <button className={to === location.pathname ? "isActive" : "isIdle"}>{children}</button>
-    </Link>
-  );
-}
+import LinkButton from "@src/components/Buttons/LinkButton";
+import { useLocation } from "react-router-dom";
 
 export default function MobileNav() {
-  const [isPath, setIsPath] = useState("/");
-
-  const handleClick = (location: any) => {
-    const path = location.pathname;
-    setIsPath(path);
-  };
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(location.pathname);
 
   return (
     <nav className="mobile-nav-bar-root">
       <div className="mobile-nav-bar-content">
-        <PathIcon to="/" handleClick={handleClick}>
+        <LinkButton
+          className={currentPath === "/" ? "isActive" : "isIdle"}
+          variant="ghostButton"
+          to="/"
+          onClick={() => setCurrentPath("/")}
+        >
           <i className="fa-solid fa-house"></i>
-        </PathIcon>
-        <PathIcon to="/webprojects" handleClick={handleClick}>
+        </LinkButton>
+        <LinkButton
+          className={currentPath === "/webprojects" ? "isActive" : "isIdle"}
+          variant="ghostButton"
+          to="/webprojects"
+          onClick={() => setCurrentPath("/webprojects")}
+        >
           <i className="fa-solid fa-laptop-code"></i>
-        </PathIcon>
-        <PathIcon to="/motionprojects" handleClick={handleClick}>
+        </LinkButton>
+        <LinkButton
+          className={currentPath === "/motionprojects" ? "isActive" : "isIdle"}
+          variant="ghostButton"
+          to="/motionprojects"
+          onClick={() => setCurrentPath("/motionprojects")}
+        >
           <i className="fa-solid fa-film"></i>
-        </PathIcon>
-        <PathIcon to="/contact" handleClick={handleClick}>
+        </LinkButton>
+        <LinkButton
+          className={currentPath === "/contact" ? "isActive" : "isIdle"}
+          variant="ghostButton"
+          to="/contact"
+          onClick={() => setCurrentPath("/contact")}
+        >
           <i className="fa-solid fa-address-book"></i>
-        </PathIcon>
-        <PathIcon to="/about" handleClick={handleClick}>
+        </LinkButton>
+        <LinkButton
+          className={currentPath === "/about" ? "isActive" : "isIdle"}
+          variant="ghostButton"
+          to="/about"
+          onClick={() => setCurrentPath("/about")}
+        >
           <i className="fa-solid fa-user"></i>
-        </PathIcon>
+        </LinkButton>
       </div>
     </nav>
   );
