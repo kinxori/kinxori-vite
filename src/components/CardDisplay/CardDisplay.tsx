38f3c280f941d1afe55d248ac27d4932651712a6
kinxori <116584982+kinxori@users.noreply.github.com>
2023-07-12 18:36:42 -0600

    adding re-named card component to repo

---
+++ src/components/CardDisplay/CardDisplay.tsx
@@ -0,0 +1,17 @@
+import "./_cardDisplay.css";
+
+export default function CardDisplay({ header, children, isActive, onClick, fullScreen }: any) {
+  return (
+    <div className={fullScreen ? "card-root-idle" : "card-root-active"}>
+      <div className="card-headline" onClick={onClick}>
+        <h3 className="card-headline-title">{header}</h3>
+        <div>
+          <span className={isActive ? "card-root-chevron-open" : "card-root-chevron-close"}>
+            <i className="fa-solid fa-chevron-right"></i>
+          </span>
+        </div>
+      </div>
+      <div className={isActive ? "card-body-open" : "card-body-close"}>{children}</div>
+    </div>
+  );
+}
